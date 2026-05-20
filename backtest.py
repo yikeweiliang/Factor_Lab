39382049f331497c201f0de384a5b77bf9467573
yikeweiliang <yikezzh@gmail.com>
2026-05-21 02:39:55 +0800

    完成了回测模块的核心代码，并加了一段测试代码

--- backtest.py
+++ backtest.py
@@ -2,36 +2,76 @@ import pandas as pd
 import numpy as np
 
 class backtest:
-    def __init__(self, initial_cash: float, initial_weights: np.ndarray, restrictions: np.ndarray, expect_weights: np.ndarray, price: np.ndarray):
-        self.initial_cash = initial_cash
+    """This class aims to realize an absolutely safe package-modifying action. What you need to input is the current money, current weights, restrictions, expected weights and price. The output is the actual weights and shares after the modification."""
+    def __init__(self, initial_money: float, initial_weights: np.ndarray, restrictions: np.ndarray, expect_weights: np.ndarray, price: np.ndarray):
+        self.initial_money = initial_money
         self.initial_weights = initial_weights
         self.restrictions = restrictions
         self.expect_weights = expect_weights
         self.price = price
     
-    def run_backtest(self):
+    def run_backtest_onetime(self):
         change_weight = self.expect_weights - self.initial_weights
         change_weight[~self.restrictions] = 0
         stock_change_weights = change_weight[1:]
+        stock_actual_change_weight = np.zeros(len(stock_change_weights))
         stock_prices = self.price[1:]  
+        current_cash = self.initial_money * self.initial_weights[0]
 
-        buy_mask = stock_change_weights > 0
-        buy_weights = stock_change_weights[buy_mask] 
-        buy_prices = stock_prices[buy_mask]  
+        sell_mask = stock_change_weights < 0
+        sell_weights = - stock_change_weights[sell_mask] 
+        sell_prices = stock_prices[sell_mask]  
 
-        theory_amounts = self.initial_cash * buy_weights  
+        sell_shares = (self.initial_money * sell_weights) // sell_prices 
 
-        buy_shares = theory_amounts // buy_prices  
+        actual_sell_amounts = sell_shares * sell_prices
+        
+        get_cash = np.sum(actual_sell_amounts)
 
-        actual_amounts = buy_shares * buy_prices 
+        current_cash += get_cash
 
-        remainders = theory_amounts - actual_amounts  
+        stock_actual_change_weight[sell_mask] = -actual_sell_amounts / self.initial_money
+        
+        if current_cash > self.expect_weights[0] * self.initial_money:
+            free_cash = current_cash - self.expect_weights[0] * self.initial_money
+            buy_mask = stock_change_weights > 0
+            buy_weights = stock_change_weights[buy_mask]
+            buy_prices = stock_prices[buy_mask]
+            if np.sum(buy_weights) > 0:
+                buy_weights /= np.sum(buy_weights)
+                buy_shares = buy_weights * free_cash // buy_prices
+                actual_buy_amounts = buy_shares * buy_prices
+                current_cash -= np.sum(actual_buy_amounts)
+                actual_buy_change_weight = actual_buy_amounts / self.initial_money
+            
+                stock_actual_change_weight[buy_mask] = actual_buy_change_weight
+        cash_weight = current_cash / self.initial_money
+        current_weights = self.initial_weights.copy()
+        current_weights[1:] += stock_actual_change_weight
+        current_weights[0] = cash_weight
+        current_shares = np.zeros(len(self.initial_weights))
+        current_shares[1:] = (self.initial_money * current_weights[1:]) / stock_prices
+        current_shares[0] = current_cash
+        return current_weights, current_shares
+    
+class step_to_next_time:
+    def __init__(self, current_shares: np.ndarray, price: np.ndarray):
+        self.current_shares = current_shares
+        self.price = price
 
-# 加总所有买入金额
-        total_buy_amount = actual_amounts.sum()  # 45000
-        total_remainder = remainders.sum()  # 0
+    def step(self):
+        current_values = self.current_shares.copy()
+        current_values[1:] *= self.price[1:]
+        current_money = np.sum(current_values)
+        current_weights = current_values / current_money
+        return current_money, current_weights
 
-# 或者直接一步到位
-        total_buy = np.sum((self.initial_cash * buy_weights) // buy_prices * buy_prices)
 
-        print(f"总买入金额: {total_buy}")
+
+
+
+
+
+
+
+         
